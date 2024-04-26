9e08e723ac7f497f806d6ad1b625d32bd1b7b755
nramani611 <nramani611@gmail.com>
2024-04-26 17:42:05 -0400

    Adding MLP

---
+++ MLP.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MLP(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+
+        self.linear1 = nn.Linear(in_channels, hidden_channels)
+        self.linear2 = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = nn.ReLU(x)
+        x = self.linear2(x)
+        x = F.softmax(x)
+        return x
